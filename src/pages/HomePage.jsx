6d26d93518675102f52442070b0de754384df236
Lexa Huang <sixian.duidui@gmail.com>
2023-11-22 03:08:01 -0500

    Implement Delete Facility

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,7 +14,14 @@ LabelDisplay.propTypes = {
   value: PropTypes.string.isRequired,
 };
 
-const EditModal = ({ isOpen, onClose, facility, closeModal, onUpdate, operationType }) => {
+const EditModal = ({
+  isOpen,
+  onClose,
+  facility,
+  closeModal,
+  onUpdate,
+  operationType,
+}) => {
   if (!isOpen) return null;
 
   return (
@@ -28,7 +35,7 @@ const EditModal = ({ isOpen, onClose, facility, closeModal, onUpdate, operationT
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-7 h-7 absolute top-3 right-3 cursor-pointer"
+          className="w-7 h-7 absolute top-3 right-3 cursor-pointer hover:text-red-500"
         >
           <path
             strokeLinecap="round"
@@ -38,7 +45,6 @@ const EditModal = ({ isOpen, onClose, facility, closeModal, onUpdate, operationT
         </svg>
 
         <div className="w-full">
-          {/* FacilityForm Component */}
           <FacilityForm
             facility={facility}
             closeModal={closeModal}
@@ -59,6 +65,56 @@ EditModal.propTypes = {
   operationType: PropTypes.string.isRequired,
 };
 
+const DeleteModal = ({ isOpen, onClose, onDeletionSuccess }) => {
+  if (!isOpen) return null;
+
+  const handleDeleteFacility = async () => {
+    try {
+      const token = localStorage.getItem("jwtToken");
+      await axios.delete("http://localhost:8080/facilities", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      onDeletionSuccess(); // Callback to parent component to update state
+      // onClose(); // Close the modal
+    } catch (error) {
+      console.error("Error deleting facility:", error.message);
+      // Optionally handle the error in UI
+    }
+  };
+
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
+      <div className="bg-white p-4 rounded-lg shadow-md w-full max-w-md mx-auto">
+        <h2 className="text-lg font-bold mb-4">Confirm Deletion</h2>
+        <p>Are you sure you want to delete this facility?</p>
+        <div className="flex justify-end mt-4">
+          <button
+            onClick={onClose}
+            className="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded-l"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={() => {
+              handleDeleteFacility();
+              onClose();
+            }}
+            className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-r"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+DeleteModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onDeletionSuccess: PropTypes.func.isRequired,
+};
+
 const HomePage = () => {
   const [facility, setFacility] = useState(null);
   const getFacility = async () => {
@@ -77,17 +133,25 @@ const HomePage = () => {
     getFacility();
   }, []);
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditModalOpen, setisEditModalOpen] = useState(false);
   const handleEditClick = () => {
-    setIsModalOpen(!isModalOpen);
+    setisEditModalOpen(!isEditModalOpen);
   };
 
   const updateFacilityDetails = (updatedFacility) => {
     setFacility(updatedFacility);
   };
 
+  // const deleteFacility = (updatedFacility) => {
+  //   setFacility(updatedFacility);
+  // };
   const handleAddClick = () => {
-    setIsModalOpen(true);
+    setisEditModalOpen(true);
+  };
+
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const handleDeleteClick = () => {
+    setIsDeleteModalOpen(true);
   };
 
   return (
@@ -99,13 +163,21 @@ const HomePage = () => {
           {facility ? (
             <div className="flex flex-col justify-between h-auto">
               <div>
+                {isDeleteModalOpen && (
+                  <DeleteModal
+                    isOpen={isDeleteModalOpen}
+                    onClose={() => setIsDeleteModalOpen(false)}
+                    onDeletionSuccess={() => setFacility(null)}
+                  />
+                )}
+
                 <EditModal
-                  isOpen={isModalOpen}
-                  onClose={() => setIsModalOpen(false)}
+                  isOpen={isEditModalOpen}
+                  onClose={() => setisEditModalOpen(false)}
                   facility={facility}
-                  closeModal={() => setIsModalOpen(false)}
-                  onUpdate={updateFacilityDetails} 
-                  operationType={"update"}
+                  closeModal={() => setisEditModalOpen(false)}
+                  onUpdate={updateFacilityDetails}
+                  operationType="update"
                 />
               </div>
               <LabelDisplay
@@ -140,43 +212,70 @@ const HomePage = () => {
                   Schedules
                 </button>
               </div>
-              {!isModalOpen && (
-                <svg
-                  onClick={() => handleEditClick()}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-7 h-7 absolute top-3 right-3 cursor-pointer hover:fill-blue-400"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                  />
-                </svg>
+              {!isEditModalOpen && (
+                <div className="absolute top-3 right-3 flex flex-col items-end">
+                  <svg
+                    onClick={() => handleEditClick()}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-8 h-8 cursor-pointer hover:fill-blue-400"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                    />
+                  </svg>
+
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    onClick={handleDeleteClick}
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-8 h-8 cursor-pointer hover:fill-red-400 mt-4 mr-0.5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                    />
+                  </svg>
+                </div>
               )}
             </div>
           ) : (
             <div className="flex justify-center items-center h-full">
-              <svg 
+              <svg
                 onClick={handleAddClick}
-                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8 cursor-pointer hover:text-blue-400"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 4.5v15m7.5-7.5h-15"
+                />
               </svg>
-              {isModalOpen && (
-              <EditModal
-                isOpen={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-                facility={facility}
-                closeModal={() => setIsModalOpen(false)}
-                onUpdate={updateFacilityDetails} 
-                operationType="add"
-              />
-            )}
+              {isEditModalOpen && (
+                <EditModal
+                  isOpen={isEditModalOpen}
+                  onClose={() => setisEditModalOpen(false)}
+                  facility={facility}
+                  closeModal={() => setisEditModalOpen(false)}
+                  onUpdate={updateFacilityDetails}
+                  operationType="add"
+                />
+              )}
             </div>
-            
           )}
         </div>
       </div>
