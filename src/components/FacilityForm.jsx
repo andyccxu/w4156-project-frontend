9df0f6080127ea76fbabdc55cfc65e73d3cc4e22
Lexa Huang <sixian.duidui@gmail.com>
2023-11-21 23:03:11 -0500

    Implement Get and Patch facility

---
+++ src/components/FacilityForm.jsx
@@ -0,0 +1,193 @@
+import { useState } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+
+const FacilityForm = ({ facility, closeModal, onUpdate }) => {
+  const [formData, setFormData] = useState({
+    facilityName: facility.facilityName || "",
+    facilityType: facility.facilityType || "",
+    operatingHoursStart: facility.operatingHours?.start || "",
+    operatingHoursEnd: facility.operatingHours?.end || "",
+    numberEmployees: facility.numberEmployees || "",
+    numberShifts: facility.numberShifts || "",
+    numberDays: facility.numberDays || "",
+  });
+
+  FacilityForm.propTypes = {
+    facility: PropTypes.shape({
+      facilityName: PropTypes.string,
+      facilityType: PropTypes.string,
+      operatingHours: PropTypes.shape({
+        start: PropTypes.string,
+        end: PropTypes.string,
+      }),
+      numberEmployees: PropTypes.string,
+      numberShifts: PropTypes.string,
+      numberDays: PropTypes.string,
+    }).isRequired,
+    closeModal: PropTypes.func.isRequired,
+    onUpdate: PropTypes.func,
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem("jwtToken"); // Replace with your token retrieval logic
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const updatedFormData = {
+        ...formData,
+        operatingHours: {
+          start: formData.operatingHoursStart,
+          end: formData.operatingHoursEnd,
+        },
+      };
+      const response = await axios.patch(
+        "http://localhost:8080/facilities",
+        updatedFormData,
+        config
+      );
+      console.log(response.data);
+      if (onUpdate) {
+        onUpdate(response.data);
+      }
+      if (closeModal) {
+        closeModal();
+      }
+    } catch (error) {
+      console.error(
+        "Error updating facility:",
+        error.response ? error.response.data : error.message
+      );
+      // Optional: Show error message to the user
+    }
+  };
+
+  return (
+    <div className="w-full max-w-xs">
+      <p className="block text-gray-700 text-lg font-bold mb-4">
+        Edit Facility
+      </p>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="facilityName"
+          >
+            Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="facilityName"
+            name="facilityName"
+            type="text"
+            placeholder={facility ? facility.facilityName : "Facility Name"}
+            value={formData.facilityName}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="facilityType"
+          >
+            Type
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="facilityType"
+            name="facilityType"
+            type="text"
+            placeholder={facility ? facility.facilityType : "Facility Type"}
+            value={formData.facilityType}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="numberShifts"
+          >
+            Shifts
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="numberShifts"
+            name="numberShifts"
+            type="number" // Assuming this should be a numeric input
+            placeholder={facility ? facility.numberShifts : "Number of Shifts"}
+            value={formData.numberShifts}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="numberDays"
+          >
+            Days
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="numberDays"
+            name="numberDays"
+            type="number" // Assuming this should be a numeric input
+            placeholder={facility ? facility.numberDays : "Number of Days"}
+            value={formData.numberDays}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Operating Hours
+          </label>
+          <div className="flex justify-between">
+            <div className="flex-1 mr-2">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="operatingHoursStart"
+                name="operatingHoursStart"
+                type="time"
+                placeholder="Start Time"
+                value={formData.operatingHoursStart}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="flex-1 ml-2">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="operatingHoursEnd"
+                name="operatingHoursEnd"
+                type="time"
+                placeholder="End Time"
+                value={formData.operatingHoursEnd}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Submit Button */}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded-full w-full sm:w-auto mt-3"
+          type="submit"
+        >
+          Save
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FacilityForm;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,51 +1,161 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import FacilityForm from "../components/FacilityForm";
+import PropTypes from "prop-types";
+
+const LabelDisplay = ({ label, value }) => (
+  <>
+    <p className="block text-gray-700 text-lg font-bold mb-2">{label}:</p>
+    <p className="text-gray-700 text-base pb-4">{value}</p>
+  </>
+);
+LabelDisplay.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
+const EditModal = ({ isOpen, onClose, facility, closeModal, onUpdate }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
+      <div className="relative p-5 border w-96 shadow-lg rounded-md bg-white mx-auto my-auto flex flex-col items-center justify-center">
+        {/* Close Icon */}
+        <svg
+          onClick={onClose}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-7 h-7 absolute top-3 right-3 cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+
+        <div className="w-full">
+          {/* FacilityForm Component */}
+          <FacilityForm
+            facility={facility}
+            closeModal={closeModal}
+            onUpdate={onUpdate}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+EditModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  facility: PropTypes.object, // Use PropTypes.shape({...}) to define specific object structure if needed
+  closeModal: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func,
+};
 
 const HomePage = () => {
-  const [facilities, setFacilities] = useState([]);
-
-  const getFacilities = async () => {
+  const [facility, setFacility] = useState([]);
+  const getFacility = async () => {
     try {
       const token = localStorage.getItem("jwtToken");
       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
       const response = await axios.get("http://localhost:8080/facilities");
-      setFacilities(response.data);
+      setFacility(response.data);
+      console.log(response.data);
     } catch (error) {
       console.error(error);
     }
   };
-
   useEffect(() => {
-    // when our application is first run
-    getFacilities();
+    getFacility();
   }, []);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleEditClick = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const updateFacilityDetails = (updatedFacility) => {
+    setFacility(updatedFacility);
+  };
+
   return (
-    // JSX code rendering the home page
-    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-5">
-      <>
-        {facilities.length > 0 ? (
-          <>
-            {/* map/loop through the schedules array */}
-            {facilities.map((facility, index) => {
-              return (
-                <div
-                  key={index}
-                  className="border-2 border-gray-200 rounded-md p-2 mb-4"
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center items-center w-1/2 max-w-screen-lg">
+        <div className="border p-4 m-4 rounded shadow w-96 relative">
+          {" "}
+          {/* Labels box */}
+          {facility ? (
+            <div className="flex flex-col justify-between h-auto">
+              <div>
+                <EditModal
+                  isOpen={isModalOpen}
+                  onClose={() => setIsModalOpen(false)}
+                  facility={facility}
+                  closeModal={() => setIsModalOpen(false)}
+                  onUpdate={updateFacilityDetails} // Added this line
+                />
+              </div>
+              <LabelDisplay
+                label="Name"
+                value={facility.facilityName}
+              ></LabelDisplay>
+              <LabelDisplay
+                label="Type"
+                value={facility.facilityType}
+              ></LabelDisplay>
+              <LabelDisplay
+                label="Shifts"
+                value={facility.numberShifts}
+              ></LabelDisplay>
+              <LabelDisplay
+                label="Days"
+                value={facility.numberDays}
+              ></LabelDisplay>
+              <LabelDisplay
+                label="Operating hours"
+                value={
+                  facility.operatingHours
+                    ? `${facility.operatingHours.start} - ${facility.operatingHours.end}`
+                    : "N/A"
+                }
+              />
+              <div className="flex flex-wrap justify-between mt-4 gap-2">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded-full w-full sm:w-auto">
+                  Employees
+                </button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded-full w-full sm:w-auto">
+                  Schedules
+                </button>
+              </div>
+              {!isModalOpen && (
+                <svg
+                  onClick={() => handleEditClick()}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-7 h-7 absolute top-3 right-3 cursor-pointer hover:fill-blue-400"
                 >
-                  <div className="font-bold text-lg mb-2">{facility._id}</div>
-                  <div className="font-bold text-lg mb-2">
-                    {facility.facilityType}
-                  </div>
-                </div>
-              );
-            })}
-          </>
-        ) : (
-          <div>There is no facility.</div>
-        )}
-      </>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                  />
+                </svg>
+              )}
+            </div>
+          ) : (
+            <p>No facilities</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
