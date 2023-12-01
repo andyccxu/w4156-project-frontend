e89e0551ad1c284a1778557a39f3ccbbe7eaa54d
Serdar Mamadzhanov <sitior@me.com>
2023-11-30 22:00:23 -0500

    Renamed the Add button in Schedule page

--- src/pages/SchedulesPage.jsx
+++ src/pages/SchedulesPage.jsx
@@ -225,7 +225,7 @@ const SchedulesPage = () => {
             onClick={handleAdd}
             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
           >
-            Add
+            Generate
           </button>
         </div>
       </div>
