9c302a6d7872833eb704beb50138b46202f13b54
Lexa Huang <sixian.duidui@gmail.com>
2023-12-06 18:25:07 -0500

    Implement send notifications for each schedule

--- src/pages/SchedulesPage.jsx
+++ src/pages/SchedulesPage.jsx
@@ -34,11 +34,45 @@ const DeleteConfirmationModal = ({ isOpen, onClose, onConfirm }) => {
   );
 };
 
+const NotifModal = ({ isVisible, onConfirm, onCancel }) => {
+  return (
+    isVisible && (
+      <div className="fixed inset-0 flex items-center justify-center z-50">
+        {/* Modal overlay */}
+        <div className="fixed inset-0 bg-black opacity-50"></div>
+
+        {/* Modal content */}
+        <div className="relative z-10 bg-white p-4 rounded-md shadow-md">
+          <p className="mb-6 mt-4">Send notifs to employees?</p>
+          <div className="flex justify-center">
+            <button
+              onClick={onConfirm}
+              className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 mr-2"
+            >
+              Yes
+            </button>
+            <button
+              onClick={onCancel}
+              className="bg-white-500 text-black px-4 py-2 rounded hover:bg-slate-400 border border-blue border-1"
+            >
+              No
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
 const SchedulesPage = () => {
   const [schedules, setSchedules] = useState([]);
   const [employeeNames, setEmployeeNames] = useState({});
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [scheduleToDelete, setScheduleToDelete] = useState(null);
+  const [isNotifModalVisible, setIsNotifModalVisible] = useState(false);
+  const [selectedSchedule, setSelectedSchedule] = useState(null);
+  const [employeeIds, setEmployeeIds] = useState(new Set());
+
 
   useEffect(() => {
     const fetchSchedules = async () => {
@@ -72,6 +106,8 @@ const SchedulesPage = () => {
         uniqueEmployeeIds.add(shift.employeeId);
       });
     });
+
+    setEmployeeIds(uniqueEmployeeIds);
 
     try {
       const token = localStorage.getItem("jwtToken");
@@ -184,6 +220,53 @@ const SchedulesPage = () => {
     }
   };
 
+  const handleNotifClick = (schedule) => {
+    setSelectedSchedule(schedule); // Set the selected schedule
+    setIsNotifModalVisible(true);
+  };
+
+  const handleNotifSubmit = async (schedule) => {
+    try {
+      const token = localStorage.getItem("jwtToken");
+      if (!token) {
+        throw new Error("Authorization token not found");
+      }
+  
+        for (const shift of schedule.shifts) {
+          try {
+            const message = `Hi! Your shift details are as follows:\nShift: ${shift.start}, ${shift.end}\n, Days: ${shift.days.join(", ")}`;
+            const Id = shift.employeeId;
+  
+            const notification = {
+              employeeId: Id,
+              message: message
+            };
+  
+            const response = await axios.post(
+              "http://localhost:8080/notifications",
+              notification,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+            console.log("Notification sent:", response.data);
+          } catch (error) {
+            console.error("Error sending notification for shift:", error);
+          }
+        
+      }
+  
+      setIsNotifModalVisible(false);
+  
+    } catch (error) {
+      alert("Error sending notifications: " + error.message);
+    }
+  };
+  
+  
+
   return (
     <div className="flex justify-center">
       <div className="w-1/2 max-w-screen-lg">
@@ -192,11 +275,18 @@ const SchedulesPage = () => {
           onClose={() => setIsModalOpen(false)}
           onConfirm={handleConfirmDelete}
         />
+        <NotifModal
+        isVisible={isNotifModalVisible}
+        onConfirm={() => handleNotifSubmit(selectedSchedule)}
+        onCancel={() => setIsNotifModalVisible(false)}
+      />
+       
         <h1 className="flex justify-center text-2xl font-bold p-4">
           Schedules
         </h1>
         <ul>
           {schedules.map((schedule) => (
+            
             <li key={schedule._id} className="mb-4">
               <div className="border p-4 rounded-lg shadow relative">
                 <p>
@@ -224,7 +314,24 @@ const SchedulesPage = () => {
                   </div>
                 ))}
 
-                {/* SVG Element */}
+                {/* SVG Elements */}
+
+                <svg
+                  onClick={() => handleNotifClick(schedule)}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-7 h-7 absolute top-2 right-2 cursor-pointer hover:fill-yellow-400"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
+                  />
+                </svg>
+
                 <svg
                   onClick={() => handleDeleteClick(schedule._id)}
                   xmlns="http://www.w3.org/2000/svg"
